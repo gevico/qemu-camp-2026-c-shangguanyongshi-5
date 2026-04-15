84f4b20f7ce2941ee0781f0a6bd3e869621a8b70
上官永石 <841842619@qq.com>
2026-04-15 08:19:07 +0800

    fix: 修复 20 找不到 sh 脚本的错误

---
+++ tests/test_20_mybash.sh
@@ -0,0 +1,8 @@
+myfile /workspace/exercises/20_mybash/bin/mybash
+myfile /workspace/exercises/20_mybash/obj/mybash/main.o
+
+mysed s/unix/linux/ "unix is opensource. unix is free os."
+
+mytrans /workspace/exercises/20_mybash/src/mytrans/text.txt
+
+mywc /workspace/exercises/20_mybash/src/mytrans/text.txt
